c2c3b434484a109ecf481be6f3a2e2887b710b9f
Steelwaffels <105564541+Steelwaffels@users.noreply.github.com>
2022-05-15 13:04:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -45,9 +45,24 @@ def draw_shape(x, y, side_len, side_angle, side_num):
 
 
 # function for establishing carl's settings
-
-def shape(x, y, shape_color, side_len, fill, side_angle, side_num, pen_size, turtle_vis, turtle_speed):
+def shape(x, y, side_len, fill, side_angle, side_num, pen_size, turtle_speed, turtle_vis, shape_color, canvas_color):
     carl(pen_size, turtle_speed, turtle_vis)
+
+    count4 = 1
+    while count4 == 1:
+        if canvas_color == shape_color:
+            color_check = str(turtle.textinput(f'settings for user-generated shape {count2} of {num_shapes}',
+                                               'shape color is the same as canvas color, continue? (y/n)'))
+            if color_check == 'n':
+                shape_color = str(turtle.textinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
+                                                   'color? '
+                                                   '(name of color no spaces, '
+                                                   'see https://i.stack.imgur.com/nCk6u.jpg '
+                                                   'for list of valid colors)\n'))
+            else:
+                count4 = 0
+        else:
+            count4 = 0
 
     carl.penup()
     carl.color(shape_color)
@@ -64,23 +79,20 @@ def shape(x, y, shape_color, side_len, fill, side_angle, side_num, pen_size, tur
 
 
 # requesting and executing user-generated shapes
-
-
 # user inputs number of shapes
-
 num_shapes = int(input('number of user-generated shapes?\n'))
 
 # user sets canvas settings
-
 print('settings for canvas:')
+canvas_color = str(input('background color? '
+                         '(name of color no spaces, '
+                         'see https://i.stack.imgur.com/nCk6u.jpg for list of valid colors)\n'))
 window(int(input('width of canvas? (pixels)\n')),
        int(input('height of canvas? (pixels)\n')),
        str(input('name of canvas?\n')),
-       str(input('background color? '
-                 '(name of color no spaces, see https://i.stack.imgur.com/nCk6u.jpg for list of valid colors)\n')))
+       canvas_color)
 
 # Console prompts user to switch to the turtle window, and input the settings for carl
-
 print('switch to turtle window')
 pen_size = int(turtle.numinput('settings for carl',
                                'pen size? (positive integer)\n'))
@@ -90,19 +102,15 @@ turtle_speed = int(turtle.numinput('settings for carl',
                                    'how fast should carl work? (positive integer)\n'))
 
 # Establishing variables needed later
-
 count2 = 1
 count3 = 1
 keep_size = ''
 
 # Drawing the shapes
-
-
 for i in range(int(num_shapes)):
 
     # checking user if they want to change their pen size, but only if it isn't the 1st shape drawn
     # because user just set their pen size
-
     if count3 != 1:
         keep_size = str(turtle.textinput(f'settings for user-generated shape {count2} of {num_shapes}',
                                          'keep the same pensize? (y/n)\n'))
@@ -112,18 +120,16 @@ for i in range(int(num_shapes)):
     while count == 1:
 
         # acting on user choice to keep or change pen size
-
         if keep_size == 'n':
             pen_size = int(turtle.numinput(f'settings for user-generated shape {count2} of {num_shapes}',
                                            'pen size? (positive integer)\n'))
 
         # requesting number of sides
         side_num = int(turtle.numinput(f'settings for user-generated shape {count2} of {num_shapes}',
-                                       'number of sides? (must be a positive integer, 1 to draw a straight line,'
-                                       ' 0 to draw a circle)\n'))
+                                       'number of sides? '
+                                       '(must be a positive integer, 1 to draw a line, 0 to draw a circle)\n'))
 
         # setting up variables for prompts and side angle
-
         if side_num == 0:
             shape_name = 'circle'
             shape_len = 'length of circle radius'
@@ -143,34 +149,31 @@ for i in range(int(num_shapes)):
             side_angle = 360 / side_num
 
             # preventing inputs that will break the program
-
         if side_num == 2 or side_num < 0:
             turtle.textinput(f'settings for user-generated shape {count2} of {num_shapes}',
                              'num sides must be a positive integer other than 2, 0 for circle '
                              'please press enter to try again')
 
             # running shape function with correct prompts
-
         else:
+            shape_color = str(turtle.textinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
+                                               'color? '
+                                               '(name of color no spaces,'
+                                               ' see https://i.stack.imgur.com/nCk6u.jpg for list of valid colors)\n'))
             shape(int(turtle.numinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
                                       f'x-coordinate for {shape_name} to start? {shape_loc}\n')),
                   int(turtle.numinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
                                       f'y-coordinate for {shape_name} to start? {shape_loc}\n')),
-                  str(turtle.textinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
-                                       'color? '
-                                       '(name of color no spaces,'
-                                       ' see https://i.stack.imgur.com/nCk6u.jpg for list of valid colors)\n')),
                   int(turtle.numinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
                                       f'{shape_len}? (pixels)\n')),
                   str(turtle.textinput(f'settings for user-generated {shape_name} {count2} of {num_shapes}',
-                                       'fill? (y/n)\n')), side_angle, side_num, pen_size, turtle_vis, turtle_speed)
+                                       'fill? (y/n)\n')), side_angle, side_num, pen_size, turtle_speed, turtle_vis,
+                  shape_color, canvas_color)
 
             # updating counters for while loops and conditional statements
-
             count = 0
             count2 += 1
             count3 += 1
 
 # ending session
-
 turtle.exitonclick()
